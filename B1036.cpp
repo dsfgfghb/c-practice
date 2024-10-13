2669b792ff200a77fdf65b9bd8cb150c28459985
fjy <q13467900@outlook.com>
2024-10-13 22:31:40 +0800

    1

--- B1036.cpp
+++ B1036.cpp
@@ -9,9 +9,27 @@ int main()
     cin >> m;
     for (int i = 0; i < n; i++)
     {
-        if (i)
+        if (i==0||i==n-1)
         {
-            /* code */
+            for (int j = 0; j < n; j++)
+            {
+                cout<<m;
+            }
+            cout<<endl;
+        }
+        else if (i%2==0)
+        {
+            cout<<endl;
+        }
+        else
+        {
+            cout<<m;
+            for (int j = 1; j < n-1; j++)
+            {
+                cout<<" ";
+            }
+            cout<<m<<endl;
+            
         }
         
     }
